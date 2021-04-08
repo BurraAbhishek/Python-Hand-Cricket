d8cd2c8295dfef73b89ff1579f7af57e82c8061a
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-04-08 15:20:10 +0530

    Improved code readability

--- registerformatch.py
+++ registerformatch.py
@@ -1,29 +1,43 @@
 import ast
 
+# Teams are identified by team name.
 teamname=input("Team name: ")
+team_data_file="team"+teamname+".txt"
 
-k="team"+teamname+".txt"
-
+# Check if the team file exists
 try:
-    f=open(k, 'r')
+    # Open the team file
+    f=open(team_data_file, 'r')
+    # Read its contents and store the string in variable 's'
     s=f.read()
     f.close()
-    d=ast.literal_eval(s)
-    pwod=input("Enter your team key: ")
-    if pwod == d[14]:
-        d.pop(14)
+    # Parse the team data
+    team_data=ast.literal_eval(s)
+    team_key=input("Enter your team key: ")
+    # Check if the key matches.
+    if team_key == team_data[14]:
+        team_data.pop(14)
+        # team1.txt contains the details of the team playing against the computer
+        # Copy all contents except the password from the original team file to this file.
+        # While playing, the list of players is obtained from team1.txt
         g=open("team1.txt", 'w')
-        g.write(str(d))
+        g.write(str(team_data))
         g.close()
+        # The team will play against the computer.
         print("Team",teamname,"is now ready to play a match!")
-        pwod=input("Enter your password: ")
-        h=open("gpascd.txt",'w')
-        h.write(pwod)
+        # Get user password.
+        match_key = input("Enter your password: ")
+        # This password is currently stored in plaintext. In a future release, passwords will be hashed and then stored.
+        h=open("game_pass.txt",'w')
+        h.write(match_key)
         h.close()
-        print("Your password is",pwod)
+        print("Your password is",match_key)
+        confirm_read = input("Press 'Enter' key to complete this process.")
     else:
         print("Looks like you forgot your key. Contact support for more details")
+        assume_forgotten = input()
 
+# If team file doesn't exist, we can't setup the match.
 except:
-    print("Team",teamname,"is not registered with the software.")
-
+    print("Team",teamname,"is not available. Please set up your team before proceeding.")
+    confirm_unavailable = input()
