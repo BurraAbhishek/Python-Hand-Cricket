f59a447123d3456c709bd0e88949aadd45e45094
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-04-09 12:23:43 +0530

    Add support for test cricket
    The previous commit created an interface for test cricket.
    This commit uses these interfaces to enable test matches.

--- innings/chasing.py
+++ innings/chasing.py
@@ -16,12 +16,12 @@ from modules.bowlerchoice import fieldChoice
 # opponent_netscore : Target - 1, the overall score of the opponent, including in Test Cricket.
 # batting_score : The team score, usually initialized to zero.
 # innings_data : Ball-by-ball score
-# runchoice : List of all the scoring choices available to the opponent.
+# start_message : Message displayed before innings start.
 # max_overs : Maximum number of overs available in the innings.
 # max_wickets : Maximum number of wickets in the innings.
 # is_test : Boolean: Is the game a test match (is_test = true) or a limited-over match? (is_test = false)
 
-def innSecond(team_1_array, team_2_array, innings, bat_bowl_choice, opponent_netscore, batting_score, innings_data, runchoice, max_overs, max_wickets, is_test):
+def chasingInnings(team_1_array, team_2_array, innings, bat_bowl_choice, opponent_netscore, batting_score, innings_data, start_message, max_overs, max_wickets, is_test):
     # Regenerate team details
     T1=team_1_array[0]
     T2=team_2_array[0]
@@ -49,7 +49,7 @@ def innSecond(team_1_array, team_2_array, innings, bat_bowl_choice, opponent_net
     B9=team_2_array[9]
     B10=team_2_array[10]
     B11=team_2_array[11]
-    print("Second Innings")
+    print(start_message)
     # Display the target
     print("Target:", opponent_netscore+1)
     # Score for each ball faced in each over of the second innings
@@ -98,7 +98,7 @@ def innSecond(team_1_array, team_2_array, innings, bat_bowl_choice, opponent_net
                 if bowler_stats[bowler][0]>=(max_overs/5) or bowler == bowlers_history[i-1]:
                     bowlerlist.pop(bowlerlist.index(bowler))
                     bowler=random.choice(bowlerlist)
-                    if bowler_stats[bowler][0]<(max_overs/5):
+                    if bowler_stats[bowler][0]<(max_overs/5) or is_test:
                         bowlerlist.append(bowlers_history[i-1])
                 # Each over has 6 balls
                 for j in range (1, 7):
