03a6ee19c7dd479e0b45443a37e54dcca07c69a0
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-04-10 13:31:47 +0530

    Improved code readability

--- modules/bowling.py
+++ modules/bowling.py
@@ -1,26 +1,26 @@
 import random
 
 # Player is bowling
-def playOut(bowler,batter):
+def playOut(bowler, batter):
     print("This is hand cricket batting. Bowler:", bowler, ". Batter on strike:", batter)
     print("Any other input means free sixer for opponent.")
     # Get a number from 0 to 6, from the player.
-    user_bowl_attempt=input("Bowl any number from 0 to 6: ")
+    user_bowl_attempt = input("Bowl any number from 0 to 6: ")
     # p_run: Player's attempted score
     try:
-        p_run=int(user_bowl_attempt)
+        p_run = int(user_bowl_attempt)
     except:
-        p_run=6
+        p_run = 6
     # Get the batter's attempted score
     # o_run: Opponent's score
-    o_run=random.randint(0, 6)
+    o_run = random.randint(0, 6)
     # Punish the bowler for overflow case
-    if p_run<0 or p_run>6:
+    if p_run < 0 or p_run > 6:
         returned_runs='6'
     else:
         # Wicket, if the numbers match
-        if p_run==o_run:
-            returned_runs='W'
+        if p_run == o_run:
+            returned_runs = 'W'
         else:
-            returned_runs=str(o_run)
+            returned_runs = str(o_run)
     return returned_runs
