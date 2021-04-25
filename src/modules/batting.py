1ead6975bb4717ba720272b0b7d2698ccb0333b0
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-04-25 15:25:51 +0530

    Give clear instructions when human is batting

--- src/modules/batting.py
+++ src/modules/batting.py
@@ -6,12 +6,12 @@ import random
 def bat(batter, is_declare_permitted, is_batter_human):
     # Get the player scoring attempt as an input
     if(is_batter_human):
-        user_bat_attempted = input("Bat any number from 0 to 6: ")
-    else:
         print("Batting: Type your scoring choice in {0, 1, 2, 3, 4, 5, 6}")
         print("Any other input means NO RUN")
         if is_declare_permitted:
             print("Type 'Declare' to declare this innings closed.")
+        user_bat_attempted = input("Bat any number from 0 to 6: ")
+    else:
         user_bat_attempted = random.randint(0, 6)
     # p_run: Player's score
     try:
