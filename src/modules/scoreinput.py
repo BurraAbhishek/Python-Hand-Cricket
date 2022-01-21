3283b87e561cdef1e27c1c540a3bb2140353f75e
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2022-01-21 18:08:25 +0530

    improvement: Type safety

--- src/modules/scoreinput.py
+++ src/modules/scoreinput.py
@@ -1,15 +1,20 @@
 from modules.batting import bat
 from modules.bowling import bowl
 
-# Player is batting
 
+def playBall(
+    bowler: str, 
+    batter: str, 
+    is_declare_permitted: bool, 
+    bowler_human: bool, 
+    batter_human: bool
+    ) -> str:
 
-def playBall(bowler, batter, is_declare_permitted, bowler_human, batter_human):
     print("This is a hand cricket-based cricket match.")
     print("Bowler:", bowler)
     print("Batter on strike:", batter)
-    bowl_score = bowl(bowler, bowler_human)
-    bat_score = bat(batter, is_declare_permitted, batter_human)
+    bowl_score = bowl(bowler_human)
+    bat_score = bat(is_declare_permitted, batter_human)
     if(bowl_score == bat_score):
         outcome = 'W'
     elif(bowl_score == '7'):
