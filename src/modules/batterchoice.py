cba4334dd80ad7645554b55e49525d082ec8e592
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2022-01-21 19:45:01 +0530

    improvement: Type safety and remove unused args
    This commit removes the unused arguments from batterChoice function

--- src/modules/batterchoice.py
+++ src/modules/batterchoice.py
@@ -3,11 +3,9 @@ import random
 # Select the batters
 
 
-def batterChoice(batter_list,
-                 non_striker,
-                 innings,
-                 user_choice_batfield,
-                 is_batter_human):
+def batterChoice(batter_list: list,
+                 non_striker: str,
+                 is_batter_human: bool) -> str:
     if not is_batter_human:
         chosen_batter = random.choice(batter_list)
     else:
@@ -31,10 +29,10 @@ def batterChoice(batter_list,
             chosen_batter = filterBatter(batter_list_dirty=batter_list)
     # Remove the chosen batter from the list of remaining batters
     batter_list.pop(batter_list.index(chosen_batter))
-    return chosen_batter
+    return str(chosen_batter)
 
 
-def cleanBatterList(batter_list):
+def cleanBatterList(batter_list: list) -> list:
     batter_list_clean = []
     for i in batter_list:
         if(len(i) > 0):
@@ -42,10 +40,10 @@ def cleanBatterList(batter_list):
     return batter_list_clean
 
 
-def filterBatter(batter_list_dirty):
+def filterBatter(batter_list_dirty: list) -> str:
     batter_list_clean = cleanBatterList(batter_list_dirty)
     if(len(batter_list_clean) > 0):
         returned_batter = random.choice(batter_list_clean)
     else:
         returned_batter = ''
-    return returned_batter
+    return str(returned_batter)
